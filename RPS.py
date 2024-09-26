9c6613addc6c16442c511b4e7928e7913daef327
mohamedaoukicha <mohamedaoukicha18@gmail.com>
2024-09-26 19:25:43 +0100

    Update RPS.py
    aucun

--- RPS.py
+++ RPS.py
@@ -1,10 +1,29 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
-
 def player(prev_play, opponent_history=[]):
     opponent_history.append(prev_play)
-
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
-
-    return guess
+    prochaine_coup = 'R'
+    if ((len(opponent_history) % 5 == 0) or (len(opponent_history) % 5 == 1)):
+        if(opponent_history[-1] == 'P'):
+            if(opponent_history[-2] == 'R'):
+                
+                prochaine_coup = 'S'
+            else:
+                prochaine_coup = 'S'
+        else:
+            prochaine_coup = 'P'
+    elif ((len(opponent_history) % 5 == 2) or (len(opponent_history) % 5 == 3)):
+        if(opponent_history[-1] == 'S'):
+            if(opponent_history[-2] == 'R'):   
+                    prochaine_coup = 'S'
+            else:
+                prochaine_coup ='R'
+        else :
+            prochaine_coup = 'S'
+    elif(len(opponent_history) % 5 == 4):
+        if(opponent_history[-1] == 'R'):
+            if(opponent_history[-2] == 'R'):
+                prochaine_coup = 'R'
+            else:
+                prochaine_coup = 'P'
+        else:
+            prochaine_coup = 'R'
+    return prochaine_coup
